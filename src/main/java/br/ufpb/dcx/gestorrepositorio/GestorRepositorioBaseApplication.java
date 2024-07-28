43ad444607017209b86367b056e418c7dd6f93c3
Daniel Faustino <daniel@dcx.ufpb.br>
2024-07-28 15:15:43 -0300

    Adding project

---
+++ src/main/java/br/ufpb/dcx/gestorrepositorio/GestorRepositorioBaseApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.gestorrepositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorRepositorioBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorRepositorioBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufpb/dcx/gestorrepositorio/GestorRepositorioBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.gestorrepositorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorRepositorioBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
